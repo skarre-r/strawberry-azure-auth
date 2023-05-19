0bb703e34aa612477c3e0330f374abd464f3694a
skarre-r <skarre-r@protonmail.com>
2023-05-19 14:20:17 +0200

    chore: update the 'strawberry_azure_auth/__init__.py' file

--- strawberry_azure_auth/__init__.py
+++ strawberry_azure_auth/__init__.py
@@ -1,3 +1,6 @@
 __version__ = "0.0.0"
 
-__all__ = []
+__all__ = ["AzureAuthExtension", "RoleBasedPermission"]
+
+from .extension import AzureAuthExtension
+from .permissions import RoleBasedPermission
