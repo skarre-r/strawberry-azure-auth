cf995817d2d55d6bca52d5d83336b3f13364b3ce
skarre-r <skarre-r@protonmail.com>
2023-05-19 12:34:41 +0200

    chore: add strawberry context types

--- strawberry_azure_auth/types.py
+++ strawberry_azure_auth/types.py
@@ -1,3 +1,15 @@
 from __future__ import annotations
 
-__all__ = []
+__all__ = ["Context", "ExecutionContext", "Info"]
+
+from typing import TypeVar, TypeAlias
+from strawberry.types.info import Info as StrawberryInfo, RootValueType
+
+from .channels.context import ChannelsContext, ChannelsExecutionContext
+from .django.context import DjangoContext, DjangoExecutionContext
+
+
+Context = TypeVar("Context", ChannelsContext, DjangoContext)
+ExecutionContext = TypeVar("ExecutionContext", ChannelsExecutionContext, DjangoExecutionContext)
+
+Info: TypeAlias = StrawberryInfo[Context, RootValueType]
