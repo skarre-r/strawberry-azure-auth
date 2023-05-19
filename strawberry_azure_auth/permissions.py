2c6d9f9d8b1a082539b86615f5928b3246c35c9e
skarre-r <skarre-r@protonmail.com>
2023-05-19 13:59:33 +0200

    feat: add base class for creating azure ad role based permission classes

--- strawberry_azure_auth/permissions.py
+++ strawberry_azure_auth/permissions.py
@@ -1,3 +1,37 @@
 from __future__ import annotations
 
-__all__ = []
+__all__ = ["RoleBasedPermission"]
+
+from typing import Any
+from strawberry.permission import BasePermission
+
+from .types import Info
+
+
+class RoleBasedPermission(BasePermission):
+    """
+    Base class for creating Azure AD role based permissions.
+
+    Example:
+
+        >>> from strawberry_azure_auth.permissions import RoleBasedPermission
+        ...
+        >>> class ReadPermission(RoleBasedPermission):
+        ...     roles = ["App.Read"]
+    """
+
+    message: str
+    roles: list[str]
+
+    async def has_permission(self, source: Any, info: Info[Any], **kwargs: Any) -> bool:
+        if not info.context.authorized:
+            self.message = "Unauthorized"
+            if hasattr(info.context, "response"):
+                info.context.response.status_code = 401
+            return False
+        if not any(role in info.context.roles for role in self.roles):
+            self.message = "Forbidden"
+            if hasattr(info.context, "response"):
+                info.context.response.status_code = 403
+            return False
+        return True
--- strawberry_azure_auth/types.py
+++ strawberry_azure_auth/types.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 __all__ = ["Context", "ExecutionContext", "Info"]
 
-from typing import TypeVar, TypeAlias
-from strawberry.types.info import Info as StrawberryInfo, RootValueType
+from typing import TypeVar, TypeAlias, Any
+from strawberry.types.info import Info as StrawberryInfo
 
 from .channels.context import ChannelsContext, ChannelsExecutionContext
 from .django.context import DjangoContext, DjangoExecutionContext
@@ -12,4 +12,4 @@ from .django.context import DjangoContext, DjangoExecutionContext
 Context = TypeVar("Context", ChannelsContext, DjangoContext)
 ExecutionContext = TypeVar("ExecutionContext", ChannelsExecutionContext, DjangoExecutionContext)
 
-Info: TypeAlias = StrawberryInfo[Context, RootValueType]
+Info: TypeAlias = StrawberryInfo[Context, Any]
