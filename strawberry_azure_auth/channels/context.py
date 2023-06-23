3c8e9e855ae8dbb7670df2407b885879b6cec05d
skarre-r <skarre-r@protonmail.com>
2023-06-23 16:46:00 +0200

    refactor: update the channels context classes to support strawberry v0.180.0

--- strawberry_azure_auth/channels/context.py
+++ strawberry_azure_auth/channels/context.py
@@ -1,37 +1,59 @@
 from __future__ import annotations
 
-__all__ = ["ChannelsContext", "ChannelsExecutionContext"]
+__all__ = ["ChannelsWebsocketContext", "ChannelsHTTPContext", "ChannelsExecutionContext"]
 
 import dataclasses
-from strawberry.channels.context import StrawberryChannelsContext
+from typing import Any, TYPE_CHECKING
 from strawberry.types.execution import ExecutionContext
+
+if TYPE_CHECKING:
+    from strawberry.channels.handlers.base import ChannelsConsumer
+    from strawberry.channels.handlers.http_handler import ChannelsRequest
+    from strawberry.http.temporal_response import TemporalResponse
 
 
 @dataclasses.dataclass
-class ChannelsContext(StrawberryChannelsContext):
+class ChannelsHTTPContext:
+    request: ChannelsRequest
+    response: TemporalResponse
+
     upn: str | None = None
     roles: list[str] = dataclasses.field(default_factory=list)
     authorized: bool = False
 
-    _handle_authentication: bool = True
-    _handle_authorization: bool = True
+    @property
+    def access_token(self) -> str | None:
+        for header in ["authorization", "Authorization"]:
+            authorization_header: str | None = self.request.headers.get(header)
+            if authorization_header and authorization_header.lower().startswith("bearer "):
+                _, access_token = authorization_header.split(" ")
+                return access_token
+        return None
+
+
+@dataclasses.dataclass
+class ChannelsWebsocketContext:
+    request: ChannelsConsumer
+    connection_params: dict[str, Any] | None = None
+
+    upn: str | None = None
+    roles: list[str] = dataclasses.field(default_factory=list)
+    authorized: bool = False
+
+    @property
+    def ws(self) -> ChannelsConsumer:
+        return self.request
 
     @property
     def access_token(self) -> str | None:
-        # check connection params
-        authorization_param: str | None = (
-            self.connection_params.get("Authorization") if self.connection_params else None
-        )
-        if authorization_param and authorization_param.lower().startswith("bearer "):
-            _, access_token = authorization_param.split(" ")
-            return access_token
-        # check headers
-        authorization_header: str | None = self.request.headers.get("Authorization")
-        if authorization_header and authorization_header.lower().startswith("bearer "):
-            _, access_token = authorization_header.split(" ")
-            return access_token
+        if self.connection_params:
+            for param in ["authorization", "Authorization"]:
+                authorization_param: str | None = self.connection_params.get(param)
+                if authorization_param and authorization_param.lower().startswith("bearer "):
+                    _, access_token = authorization_param.split(" ")
+                    return access_token
         return None
 
 
 class ChannelsExecutionContext(ExecutionContext):
-    context: ChannelsContext
+    context: ChannelsHTTPContext | ChannelsWebsocketContext
--- strawberry_azure_auth/types.py
+++ strawberry_azure_auth/types.py
@@ -5,11 +5,11 @@ __all__ = ["Context", "ExecutionContext", "Info"]
 from typing import TypeAlias, Union, Any
 from strawberry.types.info import Info as StrawberryInfo
 
-from .channels.context import ChannelsContext, ChannelsExecutionContext
+from .channels.context import ChannelsHTTPContext, ChannelsWebsocketContext, ChannelsExecutionContext
 from .django.context import DjangoContext, DjangoExecutionContext
 
 
-Context: TypeAlias = Union[ChannelsContext, DjangoContext]
+Context: TypeAlias = Union[ChannelsHTTPContext, ChannelsWebsocketContext, DjangoContext]
 ExecutionContext: TypeAlias = Union[ChannelsExecutionContext, DjangoExecutionContext]
 
 Info: TypeAlias = StrawberryInfo[Context, Any]
